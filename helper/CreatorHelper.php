72afebd445afbfe2b4efcd1bf194967b1c770d12
Joseph Anderson <joseph_anderson@fitnyc.edu>
2023-02-24 15:57:38 -0500

    change contributors section with inverted names

---
+++ helper/CreatorHelper.php
@@ -0,0 +1,53 @@
+<?php
+namespace OmekaTheme\Helper;
+
+use Laminas\View\Helper\AbstractHelper;
+use Omeka\Api\Representation\ItemRepresentation;
+
+class CreatorHelper extends AbstractHelper
+{
+    public function __invoke(ItemRepresentation $item)
+    {
+        if ($contributors = $item->value('dcterms:contributor', ['all' => true])) {
+            $creatorList = [];
+            foreach ($contributors as $key => $contributor) {
+                if (str_contains($contributor->asHtml(), 'Author') || str_contains($contributor->asHtml(), 'Creator')) {
+                    if (!in_array($contributor, $creatorList)) {
+                        $creatorList[] = $contributor;
+                    }
+                }
+            }
+            if ($creatorList) {
+                $invertedCreatorList = [];
+                foreach ($creatorList as $creator) {
+                    // invert the names
+                    if (str_contains($creator, ',')) {
+                        // if it contains a number it is likely a birth or death year, ie Madonna, 1958- but might be exceptions :|
+                        if (preg_match('~[0-9]+~', explode(",", $creator)[1])) {
+                            $invertedName = trim(explode(",", $creator)[0]);
+                        } else {
+                            $invertedName = trim(explode(",", $creator)[1]) . ' ' . trim(explode(",", $creator)[0]);
+                        }
+
+                    } else {
+                        $invertedName = $creator;
+                    }
+
+                    $invertedCreatorList[] = $invertedName;
+                }
+                if (count($invertedCreatorList) > 1) {
+                    $last = array_pop($invertedCreatorList);
+                    $creatorString = implode(', ', $invertedCreatorList) . ' and ' . $last;
+                } else {
+                    $creatorString = implode(', ', $invertedCreatorList);
+                }
+
+                return '
+                <div class="contributors fst-italic fw-bold">
+                ' . $creatorString . '
+                </div>
+                ';
+            }
+        }
+    }
+}
--- helper/ItemDetailsHelper.php
+++ helper/ItemDetailsHelper.php
@@ -10,7 +10,7 @@ class ItemDetailsHelper extends AbstractHelper
     {
         $filterLocale = (bool) $this->getView()->siteSetting('filter_locale_values');
         $escape = $this->getView()->plugin('escapeHtml');
-        $date = $resource->value('dcterms:date', ['lang' => ($filterLocale ? [$lang, ''] : null)]);
+        $date = $resource->value('dcterms:date');
         $departments = [];
         if ($resource->resourceName() == "items") {
             foreach ($resource->itemSets() as $itemSet) {
@@ -22,7 +22,7 @@ class ItemDetailsHelper extends AbstractHelper
                 }
             }
         }
-        $types = $resource->value('dcterms:type', ['all' => true, 'lang' => ($filterLocale ? [$lang, ''] : null)]);
+        $types = $resource->value('dcterms:type', ['all' => true]);
         $class = $resource->displayResourceClassLabel();
         $typesList = [];
         foreach ($types as $type) {
@@ -36,25 +36,25 @@ class ItemDetailsHelper extends AbstractHelper
         foreach ($typesList as $type) {
             if (in_array($type, $iconList)) {
                 switch ($type) {
-              case "Text":
-                $typeIcons[] = '<i class="fas fa-file-alt" aria-hidden="true" title="Text"></i><span class="visually-hidden">Text</span>';
-                break;
-              case "Still Image":
-                $typeIcons[] = '<i class="fas fa-image" aria-hidden="true" title="Image"></i><span class="visually-hidden">Image</span>';
-                break;
-              case "Image":
-                $typeIcons[] = '<i class="fas fa-image" aria-hidden="true" title="Image"></i><span class="visually-hidden">Image</span>';
-                break;
-              case "Moving Image":
-                $typeIcons[] = '<i class="fas fa-video" aria-hidden="true" title="Video"></i><span class="visually-hidden">Video</span>';
-                break;
-              case "Sound":
-                $typeIcons[] = '<i class="fas fa-volume-up" aria-hidden="true" title="Audio"></i><span class="visually-hidden">Audio</span>';
-                break;
-                case "Collection":
-                  $typeIcons[] = '<i class="fas fa-layer-group" aria-hidden="true" title="Collection"></i><span class="visually-hidden">Collection</span>';
-                  break;
-            }
+                    case "Text":
+                        $typeIcons[] = '<i class="fas fa-file-alt" aria-hidden="true" title="Text"></i><span class="visually-hidden">Text</span>';
+                        break;
+                    case "Still Image":
+                        $typeIcons[] = '<i class="fas fa-image" aria-hidden="true" title="Image"></i><span class="visually-hidden">Image</span>';
+                        break;
+                    case "Image":
+                        $typeIcons[] = '<i class="fas fa-image" aria-hidden="true" title="Image"></i><span class="visually-hidden">Image</span>';
+                        break;
+                    case "Moving Image":
+                        $typeIcons[] = '<i class="fas fa-video" aria-hidden="true" title="Video"></i><span class="visually-hidden">Video</span>';
+                        break;
+                    case "Sound":
+                        $typeIcons[] = '<i class="fas fa-volume-up" aria-hidden="true" title="Audio"></i><span class="visually-hidden">Audio</span>';
+                        break;
+                    case "Collection":
+                        $typeIcons[] = '<i class="fas fa-layer-group" aria-hidden="true" title="Collection"></i><span class="visually-hidden">Collection</span>';
+                        break;
+                }
             } else {
                 $typeLabels[] = $type;
             }
@@ -67,7 +67,7 @@ class ItemDetailsHelper extends AbstractHelper
         if ($departments) {
             $html .= '<ul class="list-inline mb-0">';
             foreach ($departments as $department) {
-                $html .= '<li class="type list-inline-item">'. $department .'</li>';
+                $html .= '<li class="type list-inline-item">' . $department . '</li>';
             }
             $html .= '</ul>';
         }
@@ -75,18 +75,18 @@ class ItemDetailsHelper extends AbstractHelper
             $html .= '<ul class="list-inline mb-0">';
             if ($typeIcons) {
                 foreach ($typeIcons as $icon) {
-                    $html .= '<li class="type list-inline-item">'. $icon .'</li>';
+                    $html .= '<li class="type list-inline-item">' . $icon . '</li>';
                 }
             }
             if ($typeLabels) {
                 foreach ($typeLabels as $label) {
-                    $html .= '<li class="type list-inline-item">'. $label .'</li>';
+                    $html .= '<li class="type list-inline-item">' . $label . '</li>';
                 }
             }
             $html .= '</ul>';
         }
 
         $html .= '</div>';
-        return  $html;
+        return $html;
     }
 }
